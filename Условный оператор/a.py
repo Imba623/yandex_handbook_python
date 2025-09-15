0aeb7d3cfbfb1870a0b2fbebfc0bd818bd1b183a
Alex <72447021+Imba623@users.noreply.github.com>
2025-09-15 17:50:05 +1000

    Add files via upload

---
+++ Условный оператор/a.py
@@ -0,0 +1,11 @@
+name = input()
+dela = input()
+
+print('Как Вас зовут?')
+print(f'Здравствуйте, {name}!')
+print('Как дела?')
+
+if dela == 'хорошо':
+    print('Я за Вас рада!')
+elif dela == 'плохо':
+    print('Всё наладится!')
