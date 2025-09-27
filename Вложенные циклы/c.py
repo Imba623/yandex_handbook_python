f0d07f263eafe7e1fe4817820e3f2ded038f947b
Alex <72447021+Imba623@users.noreply.github.com>
2025-09-27 16:37:28 +1000

    Add files via upload

--- Вложенные циклы/c.py
+++ Вложенные циклы/c.py
@@ -1,3 +1,12 @@
-n = int(input())
-count = 0
-max co
+max_number = int(input())
+row = 1 
+col = 1  
+number = 1 
+while number <= max_number:
+    while col <= row and number <= max_number:
+        print(number, end=' ')  
+        col += 1  
+        number += 1  
+    print()
+    row += 1
+    col = 1
